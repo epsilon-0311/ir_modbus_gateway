a826d0d506eaa02f1b90332291f3edcd48d31174
LehrChristoph <christoph.lehr@gmx.at>
2021-10-23 11:28:28 +0200

    added usage of NEC and EPSON commands, implemented usage of new commands

--- code/Infrared Modbus Gateway/src/main.cpp
+++ code/Infrared Modbus Gateway/src/main.cpp
@@ -14,7 +14,7 @@
 //Modbus Registers Offsets
 #define IR_REG 100
 //Used Pins
-#define IR_PIN 4 //GPIO D2
+#define IR_PIN 14 //GPIO D5
 
 // #define USE_SERIAL
 
@@ -65,31 +65,82 @@ void loop() {
     uint16_t cmd = mb.Hreg(IR_REG);
     if(last_cmd != cmd)
     {
-      last_cmd = cmd;
+        last_cmd = cmd;
 #endif
-      Serial.printf("Send CMD : %c - ", cmd);
-      if(cmd == '1' || cmd == 1)
-      {
-          Serial.println("Power On");
-          irsend.sendRaw(pwr_on, pwr_on_len, ir_frequency);
-      }
-      else if(cmd == '2' || cmd == 2)
-      {
-          Serial.println("Power Off");
-          irsend.sendRaw(pwr_off, pwr_off_len, ir_frequency);
-      }
-      else if(cmd == '3' || cmd == 3)
-      {
-          Serial.println("Next Source");
-          irsend.sendRaw(next_src, next_src_len, ir_frequency);
-      }
-      else if(cmd == '4' || cmd == 4)
-      {
-          Serial.println("Priveous Source");
-          irsend.sendRaw(priv_src, priv_src_len, ir_frequency);
-      }
-      else{
-          Serial.println("unknown");
-      }
+        Serial.printf("Send CMD : %c - ", cmd);
+        if(cmd == '1' || cmd == 1)
+        {
+            Serial.println("Power On");
+#ifdef USE_RAW_IR_DATA
+            irsend.sendRaw(pwr, pwr_len, ir_frequency);
+#else
+            irsend.sendNEC(pwr_NEC);
+#endif
+        }
+        else if(cmd == '2' || cmd == 2)
+        {
+            Serial.println("Power Off");
+#ifdef USE_RAW_IR_DATA
+            irsend.sendRaw(pwr, pwr_len, ir_frequency);
+            delay(500);
+            irsend.sendRaw(pwr, pwr_len, ir_frequency);
+#else
+            irsend.sendNEC(pwr_NEC);
+            delay(500);
+            irsend.sendNEC(pwr_NEC);
+#endif
+        }
+        else if(cmd == '3' || cmd == 3)
+        {
+            Serial.println("Set Source to HDMI");
+#ifdef USE_RAW_IR_DATA
+            irsend.sendRaw(src_hdmi, src_hdmi_len, ir_frequency);
+#else
+            irsend.sendNEC(src_hdmi_NEC);
+#endif
+        }
+        else if(cmd == '4' || cmd == 4)
+        {
+            Serial.println("Set Source to VGA");
+#ifdef USE_RAW_IR_DATA
+            irsend.sendRaw(src_vga, src_vga_len, ir_frequency);
+#else
+            irsend.sendNEC(src_vga_NEC);
+#endif
+        }
+        else if(cmd == '5' || cmd == 5)
+        {
+            Serial.println("Volume Down");
+#ifdef USE_RAW_IR_DATA
+            irsend.sendRaw(volume_down, volume_down_len, ir_frequency);
+#else
+            irsend.sendEpson(volume_down_epson);
+#endif
+        }
+        else if(cmd == '6' || cmd == 6)
+        {
+            Serial.println("Volume Up");
+#ifdef USE_RAW_IR_DATA
+            irsend.sendRaw(volume_up, volume_up_len, ir_frequency);
+#else
+            irsend.sendEpson(volume_up_epson);
+#endif
+        }
+        else if(cmd == '7' || cmd == 7)
+        {
+            Serial.println("Set Volume 0");
+            for(uint8_t i=0; i<10;i++ )
+            {
+#ifdef USE_RAW_IR_DATA
+                irsend.sendRaw(volume_down, volume_down_len, ir_frequency);
+#else
+                irsend.sendEpson(volume_down_epson);
+#endif
+                delay(100);
+            }
+        }
+        else{
+            Serial.println("unknown");
+        }
     }
 }
